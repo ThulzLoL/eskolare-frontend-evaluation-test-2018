48bcdd432a2ed202def1e31aa41352bafc224a7e
Thulz <thulz.lol@gmail.com>
2018-06-08 14:33:42 -0300

    Adicionado a estrutura das paginas e o seus routes

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -16,12 +16,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import OutPage from 'containers/OutPage/OutPage'
+import MainPage from 'containers/MainPage/MainPage'
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/main" component={MainPage} />
+        <Route path="/out" component={OutPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
---
+++ app/containers/MainPage/MainPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+    render() {
+      return (
+          <div>
+              </div>
+      );
+    }
+  }
